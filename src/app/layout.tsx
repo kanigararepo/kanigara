e43a8d5c04c5245abffcf324ee1192d072d56a13
ketutk <ketutkrisnakertajaya@gmail.com>
2025-03-01 07:36:06 +0700

    Update: modify font loading configuration for Plus Jakarta Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,10 @@ import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const jakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
+  subsets: ["cyrillic-ext"],
+  preload: true,
 });
 
 export const metadata: Metadata = {
